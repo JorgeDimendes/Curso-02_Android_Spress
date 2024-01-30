6aed6ffead298ee47f7c31a36f4bc4a9f2acc3d4
Jordev <jorgedimendes@hotmail.com>
2024-01-30 11:52:12 -0300

    Feito alterações nos codigos

--- 02_AppFitnessTracker/app/src/main/java/com/jordev/fitnesstracker/MainActivity.kt
+++ 02_AppFitnessTracker/app/src/main/java/com/jordev/fitnesstracker/MainActivity.kt
@@ -4,15 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnImc: LinearLayout
+    //private lateinit var btnImc: LinearLayout
+    private lateinit var rvMain: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Verifica se a atividade tem uma barra de ação
+        // 01) O layout XML
+        // 02) A onde a recyclerView vai aparecer(Tela principal, tela cheia)
+        // 03) Logica - Conectar o xml da celula dentro do recyclerView + a sua quantidade de elementos
+        rvMain = findViewById(R.id.rv_main)
+
+        /*// Verifica se a atividade tem uma barra de ação
         val actionBar = supportActionBar
         // Oculta a barra de ação
         actionBar?.hide()
@@ -22,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             // Navegar para proxima tela
             val intent = Intent(this, ImcActivity::class.java)
             startActivity(intent)
-        }
+        }*/
     }
 
 
