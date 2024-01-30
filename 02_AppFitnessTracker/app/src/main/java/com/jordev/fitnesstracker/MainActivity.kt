d66e031393332863ad677854b691f512b26b13d9
Jordev <jorgedimendes@hotmail.com>
2024-01-30 13:03:32 -0300

    Adicionado Recyclerview e holder

--- 02_AppFitnessTracker/app/src/main/java/com/jordev/fitnesstracker/MainActivity.kt
+++ 02_AppFitnessTracker/app/src/main/java/com/jordev/fitnesstracker/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.jordev.fitnesstracker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +18,10 @@ class MainActivity : AppCompatActivity() {
         // 01) O layout XML
         // 02) A onde a recyclerView vai aparecer(Tela principal, tela cheia)
         // 03) Logica - Conectar o xml da celula dentro do recyclerView + a sua quantidade de elementos
+        val adapter = MainAdapter()
         rvMain = findViewById(R.id.rv_main)
+        rvMain.adapter = adapter
+        rvMain.layoutManager = LinearLayoutManager(this)
 
         /*// Verifica se a atividade tem uma barra de ação
         val actionBar = supportActionBar
@@ -32,5 +36,30 @@ class MainActivity : AppCompatActivity() {
         }*/
     }
 
+    private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>(){
+
+        // 01) Qual é o layout XML da celula especifica(item)
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            val view = layoutInflater.inflate(R.layout.main_item, parent, false)
+            return MainViewHolder(view)
+        }
+
+        // 02) Disparado toda vez que houver uma rolagem na tela e for necessario trocar o conteudo
+        //     da celula
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+
+        }
+
+        // 03) Informar quantas celulas essa listagem terá
+        override fun getItemCount(): Int {
+            return 15
+        }
+
+    }
+
+    // É a classe da celula em si!
+    private class  MainViewHolder(view : View) : RecyclerView.ViewHolder(view){
+
+    }
 
 }
